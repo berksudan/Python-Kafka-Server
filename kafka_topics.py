9a4df75e5f9177797cca4e97512d9c22dfefe4b2
Berk Sudan <berk.sudan.96@gmail.com>
2019-11-28 11:34:28 +0300

    host_ip option is extended from localhost to user-defined, code simplified

--- kafka_topics.py
+++ kafka_topics.py
@@ -1,6 +1,6 @@
 import settings
 import time
-from bash_utils import run_bash
+from bash_facade import run_bash
 
 # Bash Script prefixes for topics.
 create_topic_prefix = 'sh %s/bin/kafka-topics.sh --create ' % settings.kafka_server_dir
@@ -8,19 +8,18 @@ delete_topic_prefix = 'sh %s/bin/kafka-topics.sh --delete ' % settings.kafka_ser
 list_topics_prefix = 'sh %s/bin/kafka-topics.sh --list ' % settings.kafka_server_dir
 
 
-def create_topic(topic: str, lh_port: int, rep_factor: int = 1, partitions: int = 1):
-    run_bash(
-        create_topic_prefix + '--bootstrap-server localhost:%d --replication-factor %d --partitions %d --topic %s' % (
-            lh_port, rep_factor, partitions, topic))
+def create_topic(topic: str, host_ip: str, port: int, rep_factor: int = 1, partitions: int = 1):
+    run_bash(create_topic_prefix + '--bootstrap-server %s:%d --replication-factor %d --partitions %d --topic %s' % (
+            host_ip, port, rep_factor, partitions, topic))
 
 
-def delete_topic(topic: str, lh_port: int):  # delete.topic.enable must be true to delete.
-    run_bash(delete_topic_prefix + '--bootstrap-server localhost:%d  --topic %s' % (lh_port, topic))
+def delete_topic(topic: str, host_ip: str, port: int):  # delete.topic.enable must be true to delete.
+    run_bash(delete_topic_prefix + '--bootstrap-server %s:%d  --topic %s' % (host_ip, port, topic))
 
 
-def list_topics(lh_port: int, sleep_secs_before: float = 3):
+def list_topics(host_ip: str, port: int, sleep_secs_before: float = 3):
     time.sleep(sleep_secs_before)  # Wait for a while to ensure that topic creation (if there is any) is ended.
-    listed_topics_str = run_bash(list_topics_prefix + '--bootstrap-server localhost:%d' % lh_port, return_output=True)
+    listed_topics_str = run_bash(list_topics_prefix + '--bootstrap-server %s:%d' % (host_ip, port), return_output=True)
     print('EXISTING TOPICS:')
     if not listed_topics_str:
         return print(' No topics!')
@@ -29,11 +28,11 @@ def list_topics(lh_port: int, sleep_secs_before: float = 3):
         print(' [%d] <<%s>>' % (cnt + 1, topic))
 
 
-def unit_test(topic: str, localhost_port: int):
-    list_topics(lh_port=localhost_port)
-    create_topic(topic=topic, lh_port=localhost_port)
-    list_topics(lh_port=localhost_port)
+def unit_test(topic: str, host_ip: str, port: int):
+    list_topics(host_ip, port)
+    create_topic(topic, host_ip, port)
+    list_topics(host_ip, port)
 
 
 if __name__ == '__main__':
-    unit_test(topic='dummy_topic_1234', localhost_port=9092)
+    unit_test(topic='dummy_topic_1234', host_ip="localhost", port=9092)
--- main.py
+++ main.py
@@ -1,79 +1,16 @@
-import time
-
-from kafka import KafkaConsumer, KafkaProducer
-from kafka_topics import create_topic, list_topics
+from kafka_topics import list_topics, create_topic
 from kafka_services import KafkaServices
-from kafka_producer_consumer import KafkaProducerConsumer
-from threading import Thread
-
-
-def num_of_lines(file_path: str):
-    return sum(1 for _ in open(file_path))
-
-
-"""
-def producer_part():
-    reader = open(file='r.txt', mode='rb')
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-
-    for line in reader:
-        print('sending:%s' % line)
-        producer.send('kahve', line)
-    producer.flush()
-    reader.close()
-    for i in range(200):
-        print('*=*', i)
-        producer.send('kahve', b'end')
-    return
-
-
-def consumer_part():
-    consumer = KafkaConsumer('kahve')
-
-    writer = open(file='w.txt', mode='wb')
-    msg = next(consumer)
-    while msg.value != b'end':
-        print(' > "%s"' % msg.value.decode()[:-1], '[Offset:%d, Partition:%d]' % (msg.offset, msg.partition))
-        writer.write(msg.value)
-        msg = next(consumer)
-        if msg.value == b'end':
-            print('!!!!! > "%s"' % msg.value.decode(), '[Offset:%d, Partition:%d]' % (msg.offset, msg.partition))
-
-    print('consumer exiting...')
-    writer.close()
-"""
-
-
-def producer_part(topic: str):
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-    print('Producer is initiated..')
-
-    for i in range(1000):
-        print('sending:%d' % i)
-        producer.send(topic, str(i).encode())
-        time.sleep(2)
-    producer.flush()
-    return
-
-
-def consumer_part(topic: str):
-    consumer = KafkaConsumer(topic)
-    print('Consumer is initiated..')
-
-    for msg in consumer:
-        print(' > "%s"' % msg.value.decode()[:-1], '[Offset:%d, Partition:%d]' % (msg.offset, msg.partition))
-    print('consumer exiting...')
-
+import kafka_producer_consumer
+import settings
 
 if __name__ == '__main__':
-    kafka_services = KafkaServices()
-    kafka_services.start_all()
-    exit(22)
-    create_topic(topic='foo',lh_port=9092)
-    list_topics()
+    kafkaServices = KafkaServices(broker_prop='%s/config/server-tmp.properties' % settings.kafka_server_dir)
+    kafkaServices.restart_all()
 
-    KafkaProducerConsumer.start_consumer(topic='dummy3')
-    Thread(target=producer_part(topic='dummy3')).start()
+    create_topic(topic='Events', host_ip='192.168.10.46', port=9092)
+    create_topic(topic='Actions', host_ip='192.168.10.46', port=9092)
 
-    # Thread(target=consumer_part(topic='Events')).start()
-    # time.sleep(3)
+    list_topics(host_ip='192.168.10.46', port=9092)
+
+    kafka_producer_consumer.start_consumer(topic='Actions', host_ip="192.168.10.46", port=9092)
+    kafka_producer_consumer.start_consumer(topic='Events', host_ip="192.168.10.46", port=9092)
